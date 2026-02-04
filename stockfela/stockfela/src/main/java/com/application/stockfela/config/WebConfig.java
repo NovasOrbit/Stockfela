b59280c68ef0087ad6c9014cf768a5669978eaea
Nkanyiso Madonsela <Nkanyiso.Madonsela@standardbank.co.za>
2026-02-04 16:35:30 +0200

    cors policy for front-end

---
+++ stockfela/stockfela/src/main/java/com/application/stockfela/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.application.stockfela.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+//Configure which frontend it should accept request from
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsPolicy(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5174")
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTION")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+
+    }
+
+
+}
